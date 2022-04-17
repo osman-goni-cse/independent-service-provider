c79d0afba0c9b74dbe7064243d292538bfd281b9
osman-goni-cse <osmancse32@gmail.com>
2022-04-17 15:34:02 +0600

    Login functionality added and handle error

--- src/Pages/Login/Login/Login.js
+++ src/Pages/Login/Login/Login.js
@@ -1,11 +1,43 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { Button, Form } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
+import auth from '../../../firebase.init';
 
 const Login = () => {
 
-  const handleSubmit = () => {
+  const emailRef = useRef("");
+  const passwordRef = useRef("");
+  const navigate = useNavigate("");
+  const location = useLocation();
 
+  let errorElement;
+
+  let from = location.state?.from?.pathname || "/";
+
+  const [
+    signInWithEmailAndPassword,
+    user,
+    loading,
+    error,
+  ] = useSignInWithEmailAndPassword(auth);
+
+  if(error) {
+    errorElement = <p className="text-danger">Error: {error?.message} </p>;
+  }
+
+  if(user) {
+    navigate(from, {replace:true})
+  }
+
+  const handleSubmit = (event) => {
+
+    event.preventDefault();
+
+    const email = emailRef.current.value;
+    const password = passwordRef.current.value;
+
+    signInWithEmailAndPassword(email, password);
   }
 
   return (
@@ -14,18 +46,19 @@ const Login = () => {
       <Form onSubmit={handleSubmit}>
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Control  
+          ref={emailRef}
           type="email" 
           placeholder="Enter email" />
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="formBasicPassword">
           <Form.Control
-            
+            ref={passwordRef}
             type="password"
             placeholder="Password"
           />
         </Form.Group>
-
+        {errorElement}
         <Button
           className="btn w-50 mx-auto d-block mb-3"
           variant="primary"
